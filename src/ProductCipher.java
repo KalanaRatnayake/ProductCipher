447c09db9e299a8e22072d6481b333ce86ca7c2c
Kalana Ratnayake <kalana.15@cse.mrt.ac.lk>
2018-02-26 23:46:47 +0530

    added file name input

--- src/ProductCipher.java
+++ src/ProductCipher.java
@@ -14,7 +14,9 @@ public class ProductCipher {
         Encryptor encryptor = new Encryptor("kalana");
         Decryptor decryptor = new Decryptor("kalana");
         
-        System.out.println("Enter encrypt/decrypt to proceed :");
+        System.out.println("==================== Welcome to the Product Cipher ==========================");
+        System.out.println("\n");
+        System.out.print("Enter encrypt/decrypt to proceed : ");
         String method = scanner.next();
         
         /*
@@ -22,12 +24,18 @@ public class ProductCipher {
         */
         if (method.equalsIgnoreCase("decrypt")){
             try{
-                String encryptedText = reader.readFile("encrypted.txt");
+                System.out.print("\nEnter the name of the encrypted file : " );
+                String fileName = scanner.next();
+                String encryptedText = reader.readFile(fileName + ".txt");
                 
                 String decryptedText = decryptor.decrypt(encryptedText);
+                System.out.println("\n============ Successfully decrypted ===============");
                 
-                writer.writeFile(decryptedText, "decrypted.txt");
-                System.out.println("Decryption Successful");
+                System.out.print("\nEnter the name required for the decrypted file : ");
+                String decryptFileName = scanner.next();
+                writer.writeFile(decryptedText, decryptFileName+ ".txt");
+                
+                System.out.println("\n========= Successfully Saved the file ==================");
                 
             } catch (IOException e){
                 System.out.println("File not found");  
@@ -39,12 +47,18 @@ public class ProductCipher {
         */
         } else if (method.equalsIgnoreCase("encrypt")){
             try{
-                String text = reader.readFile("text.txt");
+                System.out.print("\nEnter the name of the file that needs to be encrypted : " );
+                String originalFileName = scanner.next();
+                String text = reader.readFile(originalFileName + ".txt");
                 
                 String encryptedText = encryptor.encrypt(text);
+                System.out.println("\n================= Successfully encrypted ================");
                 
-                writer.writeFile(encryptedText, "encrypted.txt");
-                System.out.println("Encryption Succesful");
+                System.out.print("\nEnter the name required for the encrypted file : ");
+                String encryptedFileName = scanner.next();
+                writer.writeFile(encryptedText, encryptedFileName + ".txt");
+                
+                System.out.println("\n============== Succesfully Saved the file ===============");
             
             } catch (IOException e){
                 System.out.println("File not found");
